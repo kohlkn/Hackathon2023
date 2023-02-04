551a9c0e46ef8b237af128b4094d9d5f59775499
kohlkn <kohlkn2@gmail.com>
2023-02-04 17:46:07 -0500

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -24,14 +24,17 @@ export default function Home() {
                     <div><br></br></div>
                     <div class='box'>
                         <h1 class='typed'>Welcome to TrekSpec</h1>
-                        <h2>hello</h2>
+                        <h2>This is a tool to make a personalized itinerary to your liking.</h2>
                         <button class='button' onClick={handleClick}>Get Started</button>
                     </div>
                 </center>
             </header>
 
             <footer>
+                <br></br>
+                <br></br>
                 Contact
+                <br></br>
                 About Us
             </footer>
         </div>
